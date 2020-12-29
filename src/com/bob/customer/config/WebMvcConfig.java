b0dd976160b722a17d114055051958617b3560b7
bobmwangih <bobbymwangih@gmail.com>
2020-12-29 15:03:03 +0300

    added the view resolver --webMvcConfig

--- src/com/bob/customer/config/WebMvcConfig.java
+++ src/com/bob/customer/config/WebMvcConfig.java
@@ -1,5 +1,19 @@
 package com.bob.customer.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.bob")
 public class WebMvcConfig {
 
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
 }
